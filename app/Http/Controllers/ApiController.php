7e44afc1c4bc85c90682d3691a6d4ee5a01d5e0d
emmanuel <manishimweemmanuel8@gmail.com>
2020-01-22 00:17:44 +0200

    new

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -33,11 +33,11 @@ class ApiController extends Controller
 
                     if($client){
                     $data['status']="client pass";
-                return response->json($data);
+                return response()->json($data);
                         
                 }else{
                     $data['status']="ticket pass";
-                return response->json($data);
+                return response()->json($data);
 
                 }
             
